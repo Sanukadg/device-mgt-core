b00886e30281efda96a064f7e383d65c1ad28cba
Sanukadg <sanukagamage@gmail.com>
2025-10-09 16:03:00 +0530

    Add support for enrollment type reports

--- components/device-mgt/io.entgra.device.mgt.core.device.mgt.core/src/main/java/io/entgra/device/mgt/core/device/mgt/core/util/HttpReportingUtil.java
+++ components/device-mgt/io.entgra.device.mgt.core.device.mgt.core/src/main/java/io/entgra/device/mgt/core/device/mgt/core/util/HttpReportingUtil.java
@@ -70,6 +70,7 @@ public class HttpReportingUtil {
                 case Constants.BirtReporting.APP_USAGE:
                 case Constants.BirtReporting.DEVICE_INFO:
                 case Constants.BirtReporting.LOCATION_INFO:
+                case Constants.BirtReporting.ENROLLMENT_TYPE:
                     return designFile += Constants.BirtReporting.BIRT_RPT_DESIGN_EXT;
             }
         }
